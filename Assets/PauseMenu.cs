14ad8b1d67e90a468e43fab9a2889ffc7bb857df
minkyu1180 <pikachu9807@gmail.com>
2022-12-23 17:32:41 +0900

    Toy-Project

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Panel; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.SetActive(false);
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!Panel.activeSelf)
+            {
+                Time.timeScale = 0f;
+                Panel.SetActive(true);
+                Cursor.visible = true;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                Panel.SetActive(false);
+                Cursor.visible = false;
+            }
+        }   
+    }
+
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1f;
+        Panel.SetActive(false);
+        Cursor.visible = false;
+    }
+}
